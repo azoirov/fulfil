f4c2992276dab9289833569e1ba6568f72879bf0
Asadbek Zoirov <zoirovasadbek2002@gmail.com>
2022-10-17 22:11:39 +0500

    fulfil: course interface and model

---
+++ src/domain/course/course.interface.ts
@@ -0,0 +1,31 @@
+export interface ICourse {
+  _id: string;
+  title: string;
+  slug: string;
+  excerpt: string;
+  features: string[];
+  contents: string[];
+  projects: IProject[];
+  comments: ICourseComment[];
+  audiances: string[];
+  preferences: string[];
+  faqs: IFaq[];
+}
+
+export interface IProject {
+  icon: string;
+  title: string;
+}
+
+export interface ICourseComment {
+  avatar: string;
+  fullName: string;
+  title: string;
+  text: string;
+  phone: string;
+}
+
+export interface IFaq {
+  question: string;
+  answer: string;
+}
---
+++ src/domain/course/course.model.ts
@@ -0,0 +1,104 @@
+import { Document, Schema, model } from "mongoose";
+import { ICourse } from "./course.interface";
+
+const courseSchema: Schema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    excerpt: {
+      type: String,
+      required: true,
+    },
+    features: {
+      type: [String],
+      required: true,
+      default: [],
+    },
+    contents: {
+      type: [String],
+      required: true,
+      default: [],
+    },
+    projects: {
+      type: [
+        {
+          _id: false,
+          title: {
+            type: String,
+            required: true,
+          },
+          icon: {
+            type: String,
+            required: true,
+          },
+        },
+      ],
+      required: true,
+      default: [],
+    },
+    comments: {
+      type: [
+        {
+          _id: false,
+          avatar: {
+            type: String,
+            required: true,
+          },
+          fullName: {
+            type: String,
+            required: true,
+          },
+          title: {
+            type: String,
+            required: true,
+          },
+          text: {
+            type: String,
+            required: true,
+          },
+          phone: {
+            type: String,
+            required: true,
+          },
+        },
+      ],
+      default: [],
+    },
+    audiances: {
+      type: [String],
+      default: [],
+      required: true,
+    },
+    preferences: {
+      type: [String],
+      default: [],
+      required: true,
+    },
+    faqs: {
+      type: [
+        {
+          question: {
+            type: String,
+            required: true,
+          },
+          answer: {
+            type: String,
+            required: true,
+          },
+        },
+      ],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const userModel = model<ICourse & Document>("Course", courseSchema);
