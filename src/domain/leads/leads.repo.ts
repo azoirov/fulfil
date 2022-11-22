c36d653b15bb69feb41ba99f6e7998bfb7ca53c7
Bekmirzo <99716618+Bekmirzo@users.noreply.github.com>
2022-11-22 09:48:22 +0500

    Update leads.repo.ts

--- src/domain/leads/leads.repo.ts
+++ src/domain/leads/leads.repo.ts
@@ -1,25 +1,26 @@
-import { leadModel } from "./leads.model";
-import { CreateLeadDto, LeadDto } from "./dto/leads.dto";
+import {leadModel} from "./leads.model";
+import {LeadDto} from "./dto/leads.dto";
+import {Ilead} from "./leads.interface";
 
 class leadRepo {
-  public leadModel = leadModel;
+    public leadModel = leadModel;
 
-  create = async (data: CreateLeadDto): Promise<CreateLeadDto> => {
-    return this.leadModel.create(data);
-  };
+    create = async (data: Ilead): Promise<Ilead> => {
+        return this.leadModel.create(data);
+    };
 
 
-  getAll = async (): Promise<LeadDto[]> => {
-    return this.leadModel.find().lean();
-  };
+    getAll = async (): Promise<LeadDto[]> => {
+        return this.leadModel.find().lean();
+    };
 
-  getById = async (id: string): Promise<LeadDto> => {
-    return this.leadModel.findById(id);
-  };
+    getById = async (id: string): Promise<LeadDto> => {
+        return this.leadModel.findById(id);
+    };
 
-  deleteById = async (id: string): Promise<LeadDto> => {
-    return this.leadModel.findByIdAndDelete(id);
-  };
+    deleteById = async (id: string): Promise<LeadDto> => {
+        return this.leadModel.findByIdAndDelete(id);
+    };
 
 }
 
