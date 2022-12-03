26d47a276c427835d40878c386abf8658f61e2cf
MubiynDev <mubiynsafiyulloh@icloud.com>
2022-12-03 09:04:21 +0500

    created open-course section

---
+++ src/domain/open_course/open-course.controller.ts
@@ -0,0 +1,110 @@
+import { IdDto } from "@/dtos/id.dto";
+import StatusCode from "@/enums/status-code.enum";
+import { validation } from "@/utils/validation";
+import { NextFunction, Request, Response } from "express";
+import { CreateOpenCourseDto } from "./dto/open-course.dto";
+import OpenCourseService from "./open-course.service";
+
+class OpenCourseController {
+  public openCourseService = new OpenCourseService();
+
+  public create = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const createData: CreateOpenCourseDto = req.body;
+
+      await validation(CreateOpenCourseDto, createData);
+
+      const result = await this.openCourseService.create(createData);
+
+      res.status(StatusCode.Created).json({
+        data: result,
+      });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  public getAll = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const result = await this.openCourseService.getAll();
+
+      res.status(StatusCode.Ok).json({
+        data: result,
+      });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  public getBySlug = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    try {
+      const slug: string = req.params.slug;
+
+      const result = await this.openCourseService.getBySlug(slug);
+
+      res.status(StatusCode.Ok).json({
+        data: result,
+      });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  public getByPage = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    try {
+      const { page, limit } = req.query;
+
+      const result = await this.openCourseService.getByPage(page.at, limit);
+
+      res.status(StatusCode.Ok).json({
+        data: result,
+      });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  public delete = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const id: string = req.params.id;
+
+      await validation(IdDto, { id });
+
+      const result = await this.openCourseService.deleteById(id);
+
+      res.status(200).json({
+        data: result,
+      });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  public update = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const id: string = req.params.id;
+      const updateData: Partial<CreateOpenCourseDto> = req.body;
+
+      await validation(CreateOpenCourseDto, updateData, true);
+      await validation(IdDto, { id });
+
+      const result = await this.openCourseService.update(id, updateData);
+
+      res.status(StatusCode.Ok).json({
+        data: result,
+      });
+    } catch (error) {
+      next(error);
+    }
+  };
+}
+
+export default OpenCourseController;
---
+++ src/domain/open_course/open-course.repo.ts
@@ -0,0 +1,38 @@
+import { openCourseModel } from "./open-course.model";
+import { OpenCourseDto, CreateOpenCourseDto} from "./dto/open-course.dto";
+
+class OpenCourseRepo {
+  public openCourseModel = openCourseModel;
+
+  create = async (data: CreateOpenCourseDto): Promise<OpenCourseDto> => {
+    return this.openCourseModel.create(data);
+  };
+
+  getById = async (id: string): Promise<OpenCourseDto> => {
+    return this.openCourseModel.findById(id);
+  };
+
+  getBySlug = async (slug: string): Promise<OpenCourseDto> => {
+    return this.openCourseModel.findOne({
+      slug,
+    });
+  };
+
+  getByPage = async (page, limit): Promise<OpenCourseDto[]> => {
+    return this.openCourseModel.find().skip((page - 1) * 10).limit(limit)
+  };
+
+  getAll = async (): Promise<OpenCourseDto[]> => {
+    return this.openCourseModel.find().lean();
+  };
+
+  update = async (id: string, data: Partial<CreateOpenCourseDto>): Promise<OpenCourseDto> => {
+    return this.openCourseModel.findByIdAndUpdate(id, data, { new: true });
+  };
+
+  deleteById = async (id: string): Promise<OpenCourseDto> => {
+    return this.openCourseModel.findByIdAndDelete(id);
+  };
+}
+
+export default OpenCourseRepo;
